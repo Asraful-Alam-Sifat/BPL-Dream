2b03c6daba03ee5334ce8ecbc5020d40db4d9d47
Asraful Alam <asrafulalam.origin@gmail.com>
2026-03-29 11:43:06 +0600

    add NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import currencyIcon from '../../assets/Currency.png';
+
+const NavBar = () => {
+    return (
+      <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-none">
+    <button className="btn btn-square btn-ghost">
+    
+    </button>
+  </div>
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <button className="btn flex justify-between items-center">
+     0 Coins
+     <img src={currencyIcon} alt="Currency" />
+    </button>
+  </div>
+</div>
+    );
+};
+
+export default NavBar;
